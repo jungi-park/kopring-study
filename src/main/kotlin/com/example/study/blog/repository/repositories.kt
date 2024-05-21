09f3634d57de0174ea43e0473bbc60c69f17a299
박준기 <qmqqqqm@gmail.com>
2024-05-21 19:41:04 +0900

    4.4. repository 만들기

---
+++ src/main/kotlin/com/example/study/blog/repository/repositories.kt
@@ -0,0 +1,7 @@
+package com.example.study.blog.repository
+
+import com.example.study.blog.entity.WordCount
+import org.springframework.data.repository.CrudRepository
+
+interface WordRepository : CrudRepository<WordCount,String> {
+}
