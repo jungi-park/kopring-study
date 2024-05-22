bd115c7f18192f00e13988f69da898ad6a84f935
박준기 <qmqqqqm@gmail.com>
2024-05-22 18:27:04 +0900

    4.8. ExceptionHandler 만들기

--- src/main/kotlin/com/example/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/example/study/blog/service/BlogService.kt
@@ -1,6 +1,7 @@
 package com.example.study.blog.service
 
 import com.example.study.blog.dto.BlogDto
+import com.example.study.core.exception.InvalidInputException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -14,6 +15,26 @@ class BlogService {
     lateinit var restApiKey: String
 
     fun searchKakao(blogDto: BlogDto): String? {
+        val msgList = mutableListOf<ExceptionMsg>()
+
+        if (blogDto.query.trim().isEmpty()) {
+            msgList.add(ExceptionMsg.EMPTY_QUERY)
+        }
+
+        if (blogDto.sort.trim() !in arrayOf("accuracy", "recency")) {
+            msgList.add(ExceptionMsg.NOT_IN_SORT)
+        }
+
+        when {
+            blogDto.page < 1 -> msgList.add(ExceptionMsg.LESS_THAN_MIN)
+            blogDto.page > 50 -> msgList.add(ExceptionMsg.MORE_THAN_MAX)
+        }
+
+        if (msgList.isNotEmpty()){
+            val message = msgList.joinToString { it.msg }
+            throw InvalidInputException(message)
+        }
+
         val webClient = WebClient.builder().baseUrl("https://dapi.kakao.com")
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build()
 
@@ -26,4 +47,11 @@ class BlogService {
 
         return result
     }
+}
+
+private enum class ExceptionMsg(val msg: String) {
+    EMPTY_QUERY("query parameter required"),
+    NOT_IN_SORT("sort parameter one of accuracy and recency"),
+    LESS_THAN_MIN("page is less than min"),
+    MORE_THAN_MAX("page is more than max"),
 }
---
+++ src/main/kotlin/com/example/study/core/exception/InvalidInputException.kt
@@ -0,0 +1,6 @@
+package com.example.study.core.exception
+
+class InvalidInputException(
+    message: String = "Invalid Input"
+): RuntimeException(message) {
+}
